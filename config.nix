a86eb2395d31fa0df52bdc4ffad55836aad3cf84
dkool <dkool@dkools-MacBook-Pro.local>
2024-06-26 13:00:58 +0200

    change pre command execution

--- config.nix
+++ config.nix
@@ -1,15 +1,21 @@
 {
   packageOverrides = pkgs: with pkgs; {
     myPackages = pkgs.buildEnv {
-      name = "aztfmods";
+      name = "mischa-tools";
       paths = [
-        node
-        npm
+        bash-completion
         neovim
+        go
+        nodejs_22
         starship
         fd
         ripgrep
         fzf
+        lazygit
+        kubectl
+        kubectx
+        k9s
+        fluxcd
       ];
     };
   };
